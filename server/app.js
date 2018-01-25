e1126c19134f4194687f7efb390d8bc933245ef0
Ricardo Ariza <skchard09@gmail.com>
2018-01-24 20:04:17 -0500

    fix: ajuste en app.js server para entorno de productions

--- server/app.js
+++ server/app.js
@@ -10,7 +10,9 @@ const debug = new Debug('almundo:root')
 
 const app = express()
 
-app.use(cors({origin: '*'}))
+if (process.env.NODE_ENV === 'development') {
+  app.use(cors({origin: '*'}))
+}
 
 const PORT = process.env.PORT || 3001
 
@@ -18,11 +20,11 @@ app.get('/', (req, res) => {
   res.send('Run server almundo')
 })
 
-if(process.env.NODE_ENV === 'production'){
+if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(process.cwd(), 'dist')))
 }
 
-app.use('/img',express.static(path.join(__dirname, 'public/images')));
+app.use('/img', express.static(path.join(__dirname, 'public/images')));
 app.use('/api', hotels)
 
 app.listen(PORT, () => {
