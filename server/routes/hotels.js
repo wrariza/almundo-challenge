76b64c1840adc9a5b4c2450a058fa0114d89b68e
Ricardo Ariza <skchard09@gmail.com>
2018-01-27 09:42:02 -0500

    fix: ajuste al filtro servicio

--- server/routes/hotels.js
+++ server/routes/hotels.js
@@ -1,36 +1,37 @@
 import { Router } from 'express'
-import  hotels from './../data/data.json'
+import hotels from './../data/data.json'
 import _ from 'lodash'
-import { stat } from 'fs';
 
 const hotelsRouter = Router()
 
 hotelsRouter.get('/hotels', (req, res) => {
+  let dataHotels = []
   let data = []
   let query = req.query
-  let name = query.name.toUpperCase();
-  console.log(typeof(name)) 
-  let stars = String(query.stars).split(',');
+  let name = query.name.toUpperCase()
+  console.log(typeof (name))
+  let stars = String(query.stars).split(',')
 
-  if(stars[0] !== "-1"){  
+  if (stars[0] !== '-1') {
     _.each(hotels, (hotel) => {
-        if ( (stars.indexOf(String(hotel.stars)) !== -1)) {
-          data.push(hotel)
-        }
+      if ((stars.indexOf(String(hotel.stars)) !== -1)) {
+        dataHotels.push(hotel)
+      }
     })
-  }else{
-    data = hotels;
+  } else {
+    dataHotels = hotels
   }
-  
-  if(name.length > 0){
-    _.each(data, (hotel) => {
-      console.log(_.includes(hotel.name.toUpperCase(), name));
-      if ((_.includes(hotel.name.toUpperCase(), name) == true)) {
+
+  if (name.length > 0) {
+    _.each(dataHotels, (hotel) => {
+      console.log(_.includes(hotel.name.toUpperCase(), name))
+      if ((_.includes(hotel.name.toUpperCase(), name) === true)) {
         data.push(hotel)
       }
     })
+  } else {
+    data = dataHotels
   }
- 
 
   data = _.uniqBy(data, 'name')
   data = _.orderBy(data, ['stars'], ['desc'])
